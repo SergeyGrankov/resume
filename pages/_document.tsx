239ea741aecd7d37d93f0f96ae067aa52084aeca
SergeyGrankov <sergegrank0v@gmail.com>
2024-07-28 00:48:03 +0300

    Добавлен material-ui mui, подправлен линтер

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Head, Html, Main, NextScript } from 'next/document';
+import { DocumentProps, Head, Html, Main, NextScript } from 'next/document';
+import type { DocumentHeadTagsProps } from '@mui/material-nextjs/v13-pagesRouter';
+import {
+  documentGetInitialProps,
+  DocumentHeadTags,
+} from '@mui/material-nextjs/v13-pagesRouter';
 
-export default function Document() {
+export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <DocumentHeadTags {...props} />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -12,3 +19,9 @@ export default function Document() {
     </Html>
   );
 }
+
+Document.getInitialProps = async (ctx: any) => {
+  const finalProps = await documentGetInitialProps(ctx);
+
+  return finalProps;
+};
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
+import { Roboto } from 'next/font/google';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
 
 import { storeWrapper } from './model/store';
 
 import '#/global.scss';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
 
 export default function App({ Component, ...rest }: AppProps) {
   const { store, props } = storeWrapper.useWrappedStore(rest);
@@ -12,7 +27,13 @@ export default function App({ Component, ...rest }: AppProps) {
 
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <AppCacheProvider {...pageProps}>
+        <ThemeProvider theme={theme}>
+          <main className={roboto.className}>
+            <Component {...pageProps} />
+          </main>
+        </ThemeProvider>
+      </AppCacheProvider>
     </Provider>
   );
 }
